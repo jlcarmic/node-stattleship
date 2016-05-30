e1a81902d8af310cd8744694b0f2b7d41bf8c29a
John Carmichael <jlcarmic@gmail.com>
2016-05-30 15:17:57 -0400

    [#3] Add Pagination Support
    * Refactored Stattleship class: DRY'd up functions and added proper class structure
    * Added recursion to API calls to handle pagination
    * Added Q library for use in recursion
    * Updated test endpoints to support pagination changes
    
    Closes #3
    Current design does not return sideloaded data, will revisit in future issue.
    
    https://github.com/jlcarmic/node-stattleship/issues/3

--- index.js
+++ index.js
@@ -1,101 +1,233 @@
 var os = require('os');
+var Q = require('q');
 var rp = require('request-promise');
 var util = require('util');
 var version = require('./package.json').version;
 
+// Private Vars
+var options = {
+  method: 'GET',
+  uri: '',
+  json: true,
+  resolveWithFullResponse: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': '',
+    'Accept': 'application/vnd.stattleship.com; version=1',
+    'User-Agent': util.format('Stattleship-Node/%s (%s)', version, platform)
+  },
+  qs: {}
+};
 var platform = util.format('%s-%s%s', os.arch(), os.platform(), os.release());
 
-module.exports = function(key) {
-  // Set the key
-  this.key = key;
+// Private Functions
+function callEndPoint(sport, league, endpoint, params, paginate, data, total) {
+  var deferred = Q.defer();
+  options.uri = util.format("https://www.stattleship.com/%s/%s/%s", sport, league, endpoint);
+  options.qs = params;
+  options.qs.page = options.qs.page || 1;
+  console.log(options.qs.page);
 
-  // Set the options
-  this.options = {
-    method: 'GET',
-    uri: '',
-    json: true,
-    resolveWithFullResponse: true,
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': util.format('Token token=%s', this.key),
-      'Accept': 'application/vnd.stattleship.com; version=1',
-      'User-Agent': util.format('Stattleship-Node/%s (%s)', version, platform)
-    },
-    qs: {}
-  };
+  rp(options).then(function(response) {
+    total = response.headers.total;
+    console.log(total);
+    data = data.concat(response.body[endpoint]);
+    console.log(Object.keys(data).length);
+    options.qs.page++;
 
-  this.callEndPoint = function(sport, league, endpoint, params) {
-    this.options.uri = util.format("https://www.stattleship.com/%s/%s/%s", sport, league, endpoint);
-    this.options.qs = params;
+    // Page is 0 if use did not specify specific page, if they did no need to recurse
+    if(paginate && Object.keys(data).length < total) {
+      callEndPoint(sport, league, endpoint, params, paginate, data, total).then(function(data) {
+        deferred.resolve(data);
+      });
+    } else {
+      deferred.resolve(data);
+    }
+  });
 
-    return rp(this.options).then(function(response) {
-      return response.body[endpoint];
-    }).catch(function(err) {
-      throw (err);
-    });
-  };
+  return deferred.promise;
+}
 
-  this.feats = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "feats", params);
-  };
+// Object
+function Stattleship(token) {
+  options.headers.Authorization = util.format('Token token=%s', token);
+}
 
-  this.game_logs = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "game_logs", params);
-  };
+// Public Functions
+Stattleship.prototype.feats = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
 
-  this.games = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "games", params);
-  };
-
-  this.injuries = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "injuries", params);
-  };
-
-  this.penalties = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "penalties", params);
-  };
-
-  this.players = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "players", params);
-  };
-
-  this.rankings = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "rankings", params);
-  };
-
-  this.rosters = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "rosters", params);
-  };
-
-  this.scoring_plays = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "scoring_plays", params);
-  };
-
-  this.stat_leaders = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "stat_leaders", params);
-  };
-
-  this.stats = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "stats", params);
-  };
-
-  this.team_game_logs = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "team_game_logs", params);
-  };
-
-  this.team_outcome_streaks = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "team_outcome_streaks", params);
-  };
-
-  this.teams = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "teams", params);
-  };
-
-  this.top_stats = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "top_stats", params);
-  };
-
-  this.total_stats = function(sport, league, params) {
-    return this.callEndPoint(sport, league, "total_stats", params);
-  };
+  return callEndPoint(sport, league, "feats", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
 };
+
+Stattleship.prototype.game_logs = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
+
+  return callEndPoint(sport, league, "game_logs", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
+};
+
+Stattleship.prototype.games = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
+
+  return callEndPoint(sport, league, "games", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
+};
+
+Stattleship.prototype.injuries = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
+
+  return callEndPoint(sport, league, "injuries", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
+};
+
+Stattleship.prototype.penalties = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
+
+  return callEndPoint(sport, league, "penalties", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
+};
+
+Stattleship.prototype.players = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
+
+  return callEndPoint(sport, league, "players", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
+};
+
+Stattleship.prototype.rankings = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
+
+  return callEndPoint(sport, league, "rankings", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
+};
+
+Stattleship.prototype.rosters = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
+
+  return callEndPoint(sport, league, "rosters", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
+};
+
+Stattleship.prototype.scoring_plays = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
+
+  return callEndPoint(sport, league, "scoring_plays", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
+};
+
+Stattleship.prototype.stat_leaders = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
+
+  return callEndPoint(sport, league, "stat_leaders", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
+};
+
+Stattleship.prototype.stats = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
+
+  return callEndPoint(sport, league, "stats", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
+};
+
+Stattleship.prototype.team_game_logs = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
+
+  return callEndPoint(sport, league, "team_game_logs", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
+};
+
+Stattleship.prototype.team_outcome_streaks = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
+
+  return callEndPoint(sport, league, "team_outcome_streaks", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
+};
+
+Stattleship.prototype.teams = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
+
+  return callEndPoint(sport, league, "teams", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
+};
+
+Stattleship.prototype.top_stats = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
+
+  return callEndPoint(sport, league, "top_stats", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
+};
+
+Stattleship.prototype.total_stats = function(sport, league, params) {
+  var data = [];
+  var total = 0;
+  var paginate = (params.page === undefined);
+
+  return callEndPoint(sport, league, "total_stats", params, paginate, data, total)
+    .then(function(data) {
+      return data;
+  });
+};
+
+module.exports = Stattleship;
